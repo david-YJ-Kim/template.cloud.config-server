71d4c5ea674772cafe7af12d73a5e3d4ffc2a942
DavidKim <yj.david.kim@gmail.com>
2023-11-18 11:07:32 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+레퍼런스
+https://bravenamme.github.io/2020/08/16/spring-cloud-config-server/
---
+++ src/test/java/com/dk/template/configserver/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dk.template.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
